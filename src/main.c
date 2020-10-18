c978a169f645a94ffbefafc2e921443714a06ff3
Benyamin <beneslami72@gmail.com>
2020-10-18 21:55:48 +0200

    update

--- src/main.c
+++ src/main.c
@@ -13,12 +13,7 @@
 #define TRUE      1
 #define FALSE     0
 #define N         6
-#define IP1       127.0.0.1
-#define IP2       127.0.0.1
-#define IP3       127.0.0.1
-#define IP4       127.0.0.1
-#define IP5       127.0.0.1
-#define IP6       127.0.0.1
+#define IP        127.0.0.1
 #define TABLESIZE 100
 
 struct node{
@@ -37,30 +32,33 @@ int num = 0;
 struct machine_id
 {
 	char *ip_address;
-	int tcpportno;
-	int udpportno;
+	int  tcpportno;
+	int  udpportno;
 }node[20];
 
 /* start : Hash Table functions */
 
 /*  End  : Hash Table functions */
 
-int main(void){
+int main(int argc, char *argv){
   int master_socekt, udp_socket;
   struct sockaddr_in address, server_addr_udp, client_addr;
-  int new_socket, maxfd;
+  int new_socket, maxfd, addrlen;
   char buffer[1025], replyBuffer[1024];
   fd_set readfds;
+  int node_id = atoi(argv[1]);
+  int opt = TRUE;
 
   for(int i=0; i<N; i++){
-    node[i].ip_address = "127.0.0.1";
+    node[i].ip_address = IP;    //prone to error
     node[i].tcpportno = 2000 + i*2;
     node[i].udpportno = node[i].tcpportno + 1;
   }
-  printf ("       INSTRUCTIONS \n\n  ================= NODE %d =======================\n",num);
+
+  printf ("       INSTRUCTIONS \n\n  ================= NODE %d =======================\n",node_id);
 	puts("   1.'put' request format : PUT(<integer>,<integer>)\n");
 	puts("   2.'get' request format : GET(<integer>)\n");
-	puts("   3.To print Hash Table : 'r'\n");
+	puts("   3.To print Hash Table : 'p'\n");
 	puts("-----------------------------------\n\nENTER GET/PUT REQUEST :");
 
 
@@ -70,39 +68,53 @@ int main(void){
   udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
   if(udp_socket < 0){
     perror("UDP socket error");
+    exit(EXIT_FAILURE);
   }
-  memset(&address, 0, sizeof(address));
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = htonl(INADDR_ANY);
-  address.sin_port = htons();
+
   //UDP socket attributes for current node
+  server_addr_udp.sin_family = AF_INET;
+  server_addr_udp.sin_addr.s_addr = INADDR_ANY;
+  server_addr_udp.sin_port = htons(node[node_id.udpportno]);
+  memset(&server_addr_udp, 0, sizeof(server_addr_udp));
 
   //bind UDP socket
-  int bnd = bind(udp_socket, (sockaddr*)&address, sizeof(address));
+  int bnd = bind(udp_socket, (sockaddr*)&server_addr_udp, sizeof(server_addr_udp));
   if(bnd < 0){
     perror("bind error");
+    exit(EXIT_FAILURE);
   }
-  
+
   //create master TCP socket
   master_socekt = socket(AF_INET, SOCK_STREAM, 0);
-  if(socket < 0){
+  if(master_socekt < 0){
     perror("Socket Error");
+    exit(EXIT_FAILURE);
   }
   printf("master TCP socket is created\n");
 
   //set master socket to allow multiple connections
+  if(setsockopt(master_socekt, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt))< 0){
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+  }
 
   //TCP socket attributes for current node
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(node[node_id].tcpportno);
 
   //bind TCP socket
   bnd = bind(master_socekt, (sockaddr*)&address, sizeof(address));
   if(bnd < 0){
     perror("bind error");
+    exit(EXIT_FAILURE);
   }
+
   //listen TCP socket
   int lsn = listen(master_socekt, 20);
   if(lsn < 0){
     perror("listen error");
+    exit(EXIT_FAILURE);
   }
 
   while(1){
@@ -117,13 +129,50 @@ int main(void){
     select(maxfd+1, &readfds, NULL, NULL, NULL);
 
     if(FD_ISSET(master_socekt, &readfds)){
+      addrlen = sizeof(address);
+      new_socket = accept(master_socekt, (struct sockaddr*)&address, &addrlen);
+      if(new_socket < 0){
+        perror("Accept");
+        exit(EXIT_FAILURE);
+      }
+      if(){                  // means put request was made on this node , and this node need to supply the value  //TODO
 
+        close(new_socket);
+      }
+      else{                 // means get request was made and tcp server is only need to recieve value from client
+        int read_value = read(new_socket, buffer, 1024);
+        if(read_value < 0){
+          perror("read");
+          exit(EXIT_FAILURE);
+        }
+        buffer[read_value] = '\0';
+        printf("\n%s\n",buffer);
+      }
+      close(new_socket);
     }
-    else if(FD_ISSET(udp_socket, &readfds)){
+
+    else if(FD_ISSET(udp_socket, &readfds)){   //TODO
 
     }
     else if(FD_ISSET(0, &readfds)){
+      char rec_buffer[2048];
+      gets(rec_buffer);
+      if(rec_buffer[0] == 'r' || rec_buffer[0] == 'R'){
+        DisplayTable();    //TODO
+      }
+      else if(check_if_data_is_local() == 0){ //return 1 if data is for this node, return 0 if data is not for this node
+        // check if the command is put or get --> call function get_or_put(): return 1 for get, 0 for put
 
+        // if it is a put command, then extract the value to be sent. and set the flag for put
+        // else, set the flag for get.
+        // forward the data to the next node via udp socket
+      }
+      else{
+        printf("Processing the request here\n\n");
+
+        // check if the request is put or get
+        
+      }
     }
   }
   return 0;
